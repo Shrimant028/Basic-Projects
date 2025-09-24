d628513eb2dd9d78b1d16e9c4871660f41870258
Shrimant kasode <137248851+Shrimant028@users.noreply.github.com>
2025-09-24 21:09:46 +0530

    Create Addition program.c
    Addition program in c

---
+++ Addition program.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+  int num1,num2,sum;
+
+  printf("Enter a Number : ");
+  scanf("%d", &num1);
+  printf("Enter a Number : ");
+  scanf("%d", &num2);
+
+  sum=num1+num2;
+  printf("Sum is : %d", sum);
+
+  return 0;
+}
