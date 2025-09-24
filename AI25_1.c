3ba195e38c9f17a205af1b89cf6c31f5b8611445
Shrimant kasode <137248851+Shrimant028@users.noreply.github.com>
2025-09-24 21:14:35 +0530

    Add files via upload

---
+++ AI25_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    
+    float principal, rate, time, simple_interest;
+
+    printf("Enter the principal amount: ");
+    scanf("%f", &principal);
+
+    printf("Enter the annual interest rate : ");
+    scanf("%f", &rate);
+
+    printf("Enter the time period : ");
+    scanf("%f", &time);
+
+    simple_interest = (principal * rate * time) / 100.0;
+
+    printf("\nThe Simple Interest is: %.2f\n", simple_interest);
+
+    return 0;
+    }
